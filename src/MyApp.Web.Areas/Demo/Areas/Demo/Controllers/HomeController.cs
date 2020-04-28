ba29d6dd61c153d0c3ca984b9c2eff33b265ba5b
congzw <2981810300@qq.com>
2020-04-28 14:26:45 +0800

    add dynamic area "Demo"

---
+++ src/MyApp.Web.Areas/Demo/Areas/Demo/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Web.Areas.Demo.Controllers
+{
+    [Area("Demo")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MyApp.Web.Areas/Demo/Areas/Demo/DemoStartup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using MyApp.Common.Modules;
+
+namespace MyApp.Web.Areas.Demo
+{
+    public class DemoStartup : IModuleStartup
+    {
+        public int Order { get; } = 0;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder builder)
+        {
+        }
+    }
+}
