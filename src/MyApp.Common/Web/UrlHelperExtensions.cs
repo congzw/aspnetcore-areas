5b5546f428768bda0b4bdaafc99201ce086207d0
congzw <2981810300@qq.com>
2020-04-29 10:04:07 +0800

    add area url helper

---
+++ src/MyApp.Common/Web/UrlHelperExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Common.Web
+{
+    public static class UrlHelperExtensions
+    {
+        public static string AreaContent(this IUrlHelper urlHelper, string contentPath, string area)
+        {
+            //~/Content/css/foo.css" => ~/Areas/Demo/Content/css/foo.css"
+            var areaContentPath = "~/Areas/" + area + "/" + contentPath.TrimStart('~').TrimStart('/');
+            return urlHelper.Content(areaContentPath);
+        }
+    }
+}
