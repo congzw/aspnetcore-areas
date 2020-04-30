0f3edd3efd6303525caca7fdfad8c64e3df1b6bc
congzw <2981810300@qq.com>
2020-04-30 14:17:31 +0800

    add cache key

---
+++ src/MyApp.Common/CacheKeyHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp.Common
+{
+    public class CacheKeyHelper
+    {
+        public string DefaultPrefix { get; set; } = "MyCache.";
+
+        public string CreateKey(Type theType, string prefix = null)
+        {
+            return string.Format("{0}{1}", prefix ?? DefaultPrefix, theType.FullName);
+        }
+
+        public static CacheKeyHelper Instance = new CacheKeyHelper();
+    }
+}
